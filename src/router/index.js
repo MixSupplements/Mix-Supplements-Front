fc5f6bed421d7c235999fbfe0338b915555610ba
Mohamed Elgendy <mohamedelgendyx@gmail.com>
2023-06-17 15:05:01 +0300

    add product route

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,17 @@ import { Route, Routes } from "react-router-dom";
 
 import PrimaryLayout from "../layouts/PrimaryLayout";
 import Home from "../pages/Home";
+import Product from "../pages/Product";
 
 const AppRouter = () => {
-    return ( 
+    return (
         <Routes>
             <Route element={<PrimaryLayout />}>
                 <Route path="/" element={<Home />} />
+                <Route path="/store/product/:id" element={<Product />} />
             </Route>
         </Routes>
-     );
+    );
 }
- 
+
 export default AppRouter;
