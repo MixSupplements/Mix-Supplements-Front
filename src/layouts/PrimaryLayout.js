d0bcd08a4d40449dd267b838e50471455e561eb1
Mohamed Talaat <Mohamed.Talaat.Afyfy@Gmail.com>
2023-06-17 20:36:57 +0200

    Header,Footer,Wishlist

--- src/layouts/PrimaryLayout.js
+++ src/layouts/PrimaryLayout.js
@@ -1,19 +1,19 @@
-import { Outlet } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+import { Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
-import Navbar from '../components/common/Navbar';
-import Footer from '../components/common/Footer';
+import Navbar from "../components/common/Navbar/Navbar";
+import Footer from "../components/common/Footer/Footer";
 
 const PrimaryLayout = () => {
-    return ( 
-        <>
-            <Navbar />
-            <Container>
-                <Outlet />
-            </Container>
-            <Footer />
-        </>
-     );
-}
- 
+  return (
+    <>
+      <Navbar />
+      <Container className="primaryLayout-outlet-container">
+        <Outlet />
+      </Container>
+      <Footer />
+    </>
+  );
+};
+
 export default PrimaryLayout;
