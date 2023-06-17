224ca91e74b734506cb6339127faa28970bcae34
Abdalla Yassein <47080100+AYs99@users.noreply.github.com>
2023-06-17 20:11:08 +0300

    made initial shop UI

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import PrimaryLayout from "../layouts/PrimaryLayout";
 import Home from "../pages/Home";
+import Shop from "../pages/Shop";
 
 const AppRouter = () => {
     return ( 
         <Routes>
             <Route element={<PrimaryLayout />}>
                 <Route path="/" element={<Home />} />
+                <Route path="/shop" element={<Shop />} />
             </Route>
         </Routes>
      );
