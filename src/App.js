63aea79a39e6c076dbc8cb4808a47878345fda01
Abdulrahman-Elsayes <abdosayes44@gmail.com>
2023-06-18 21:47:31 +0300

    upload sign-up and sign-in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import AppRouter from "./router";
 
 import "./App.css";
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 
 import PrimaryLayout from "../layouts/PrimaryLayout";
 import Home from "../pages/Home";
+import Login from "../pages/Login/Login";
+import Signup from "../pages/Sign-up";
 import Wishlist from "../pages/Wishlist/Wishlist";
 
 const AppRouter = () => {
@@ -9,6 +11,8 @@ const AppRouter = () => {
     <Routes>
       <Route element={<PrimaryLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
         <Route path="/wishlist" element={<Wishlist />} />
       </Route>
     </Routes>
