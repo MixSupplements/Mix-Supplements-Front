6d43aa846b108fc8286ee4f4e4a1a94e828c0df2
Abdalla Yassein <47080100+AYs99@users.noreply.github.com>
2023-06-21 18:05:01 +0300

    view cart items

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,25 +1,36 @@
 import { useEffect } from "react";
 import CartCard from "../../components/CartCard";
+import { useSelector } from "react-redux";
+
+import styles from "../../styles/userProfile/userWishlist.module.css";
 
 const Cart = () => {
     useEffect(() => {
         window.scrollTo(0,0);
-      },[])
+      },[]);
+
+    const cart = useSelector(store => store.cart);
+
+    let showItems = [];
+    let emptyCart = true;
+    if(cart.cartItems.length === 0){
+        showItems = <div className={styles["empty-placeholder"]}>No Product Found</div>;
+    }else{
+        emptyCart = false;
+        cart.cartItems.map(item => {
+            return(
+                showItems.push(<CartCard key={item.item._id} item={item.item} />)
+            )
+        })
+    }
+
     return ( 
         <div className="text-center">
             <h2>My Cart</h2>
             <div className="cart-content row pt-5">
                 <div className="cart-items text-start col-md-9">
-                    <CartCard />
-                    <CartCard />
-                    <CartCard />
-                    <CartCard />
-                    <CartCard />
-                    <CartCard />
-                    <CartCard />
-                    <CartCard />
-                    <CartCard />
-                    <button className="btn home-button">Proceed  To Checkout</button>
+                    { showItems }
+                    <button className="btn home-button" disabled={emptyCart}>Proceed  To Checkout</button>
                 </div>
                 <div className="cart-total  col-md-3">
                     <div className="border border-white p-3 text-start rounded-2">
