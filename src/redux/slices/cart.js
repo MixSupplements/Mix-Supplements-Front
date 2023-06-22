dafa9646151bbcacfb27047927d04c9e516e9970
Abdalla Yassein <47080100+AYs99@users.noreply.github.com>
2023-06-22 11:57:41 +0300

    added increase and decrease product counter by one functionality

--- src/redux/slices/cart.js
+++ src/redux/slices/cart.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const init = {
     count: 0,
+    totalPrice: 0,
     cartItems: []
 }
 const cartSlice = createSlice({
@@ -9,19 +10,43 @@ const cartSlice = createSlice({
     initialState: init,
     reducers: {
         addToCart: (state, action) => {
-            state.count = state.count + action.payload.count;
+            state.count += action.payload.count;
+            state.totalPrice += action.payload.item.price * action.payload.count;
             state.cartItems.push({item: action.payload.item, count: action.payload.count});
         },
         removeFromCart: (state, action) => {
-            state.count = state.count - action.payload.count;
             const index = state.cartItems.findIndex(item => item.item._id === action.payload.item._id);
+            state.count -= state.cartItems[index].count;
+            state.totalPrice -= action.payload.item.price * state.cartItems[index].count;
             if(index !== -1){
                 state.cartItems.splice(index, 1);
             }
+        },
+        increaseCountByOne: (state, action) => {
+            state.count += 1;
+            const index = state.cartItems.findIndex(item => item.item._id === action.payload.item._id);
+            if(index !== -1){
+                state.cartItems[index].count += 1;
+            }
+            state.totalPrice += action.payload.item.price;
+        },
+        decreaseCountByOne: (state, action) => {
+            state.count -= 1;
+            const index = state.cartItems.findIndex(item => item.item._id === action.payload.item._id);
+            if(state.cartItems[index].count === 1){
+                if(index !== -1){
+                    state.cartItems.splice(index, 1);
+                }
+            } else {
+                if(index !== -1){
+                    state.cartItems[index].count -= 1;
+                }
+            }
+            state.totalPrice -= action.payload.item.price;
         }
     }
 })
 
-export const {addToCart, removeFromCart} = cartSlice.actions;
+export const {addToCart, removeFromCart, increaseCountByOne, decreaseCountByOne} = cartSlice.actions;
 
 export default cartSlice.reducer;
