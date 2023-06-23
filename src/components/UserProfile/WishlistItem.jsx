a209297ebb481f03f0a94e72c673bd7393bee338
Abdalla Yassein <47080100+AYs99@users.noreply.github.com>
2023-06-23 20:54:12 +0300

    view wishlist items inside wishlist page

--- src/components/UserProfile/WishlistItem.jsx
+++ src/components/UserProfile/WishlistItem.jsx
@@ -2,16 +2,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartPlus, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "../../styles/userProfile/wishlistItem.module.css";
+import { useDispatch } from "react-redux";
+import { removeFromWishlist } from "../../redux/slices/wishlist";
 
-const WishlistItem = ({ item, handleRemove }) => {
+const WishlistItem = ({ item }) => {
     const currencyFormat = (price) => {
         return price.toLocaleString("en-US", { minimumFractionDigits: 2 });
     };
+    const dispatcher = useDispatch();
     return (
         <div className={`${styles["wishlist-item-container"]} card text-white`}>
             <div className="row g-0 align-items-center">
                 <div className="col-md-3">
-                    <img src={item.image[0].imageUrl} alt="product" className="img-fluid" />
+                    <img src={item.images[0].imageUrl} alt="product" className="img-fluid" />
                 </div>
                 <div className="col-md-9 card-body p-0 pt-3 p-md-0">
                     <h5 className="card-title">{item.name}</h5>
@@ -23,7 +26,7 @@ const WishlistItem = ({ item, handleRemove }) => {
                         </div>
                         <div
                             className={`btn ${styles["remove-btn"]} btn-outline-danger`}
-                            onClick={() => handleRemove(item.id)}
+                            onClick={() => {dispatcher(removeFromWishlist(item))}}
                         >
                             <FontAwesomeIcon icon={faTrashCan} />
                             <span style={{ marginLeft: "6px" }}> Remove from list</span>
