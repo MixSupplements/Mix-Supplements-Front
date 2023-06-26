11261406d254360e8d15428317cc69f91b210d54
Mohamed Elgendy <mohamedelgendyx@gmail.com>
2023-06-26 14:18:00 +0300

    add token slice and wishlist thunk

---
+++ src/redux/slices/token.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const tokenSlice = createSlice({
+    name: "tokenSlice",
+    initialState: "",
+    reducers: {
+        setToken: (state, action) => {
+            state = action.payload;
+        }
+    }
+});
+
+export const { setToken } = tokenSlice.actions;
+
+export default tokenSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from './slices/cart';
-import wishlistSlice from './slices/wishlist'
+import wishlistSlice, { getWishlist } from './slices/wishlist';
+import tokenSlice from './slices/token';
 
-export default configureStore({
-    reducer: { 
+const store = configureStore({
+    reducer: {
         cart: cartSlice,
-        wishlist: wishlistSlice
-    }
+        wishlist: wishlistSlice,
+        token: tokenSlice
+    },
 })
+
+store.dispatch(getWishlist());
+
+export default store;
