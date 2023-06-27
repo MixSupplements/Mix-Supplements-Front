e5f237c52a5deb10a4fcc33377ee4dd0f22d52db
Mohamed Talaat <Mohamed.Talaat.Afyfy@Gmail.com>
2023-06-27 02:40:40 +0200

    No products found component added

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axiosInstance from "../../APIs/config";
 import ProductCard from "../../components/ProductCard";
+import styles from "../../styles/userProfile/userWishlist.module.css";
 
 function Search() {
   const location = useLocation();
@@ -11,7 +12,6 @@ function Search() {
       .get(`/product/search/${location.pathname.split("/")[2]}`)
       .then((res) => {
         setProducts(res.data);
-        console.log(res);
       })
       .catch((error) => {
         console.log(error);
@@ -20,9 +20,13 @@ function Search() {
   return (
     <>
       <section className="row justify-content-center pt-4">
-        {products?.map((product) => {
-          return <ProductCard key={product._id} product={product} />;
-        })}
+        {products?.length > 0 ? (
+          products?.map((product) => {
+            return <ProductCard key={product._id} product={product} />;
+          })
+        ) : (
+          <div className={styles["empty-placeholder"]}>No Products Found</div>
+        )}
       </section>
     </>
   );
